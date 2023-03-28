05302e8672e079f3f743b9ec54acaf0cf9fb0c30
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-03-28 19:40:48 +0530

    added home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-
-import './App.css';
-import Login from './components/RegisterComponents/Login';
-import Navbar from './components/NavbarComponent/Navbar';
-import SignUp from './components/RegisterComponents/SignUp';
-import Adopt from './components/AdoptComponent/Adopt'
+import "./App.css";
+import Home from "./components/HomeComponents/Home";
+import Login from "./components/RegisterComponents/Login";
+import Navbar from "./components/NavbarComponent/Navbar";
+import SignUp from "./components/RegisterComponents/SignUp";
+import Adopt from "./components/AdoptComponent/Adopt";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    
-    <BrowserRouter>
-
-    <Navbar/>
-    <Routes>
-    <Route  path="/Login"element={<Login />}></Route>
-    <Route  path="/SignUp"element={<SignUp />}></Route>
-    <Route  path="/Adopt"element={<Adopt />}></Route>
-    
-
-    </Routes>
-
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/Adopt" element={<Adopt />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
