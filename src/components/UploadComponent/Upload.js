10a75cd08d104bb1597c9d19c0a916d0c4bdc13d
HARSH KHATRI <83015486+harshkhatri2220@users.noreply.github.com>
2023-04-02 01:52:59 +0530

    upload color

--- src/components/UploadComponent/Upload.js
+++ src/components/UploadComponent/Upload.js
@@ -83,7 +83,7 @@ const Upload = () => {
                         <label for="terms" class="font-light text-gray-500 :text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline :text-primary-500" href="#">Terms and Conditions</a></label>
                       </div>
                   </div>
-                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center :bg-primary-600 :hover:bg-primary-700 :focus:ring-primary-800">Upload</button>
+                  <button type="submit" class="w-full  bg-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center :bg-primary-600 hover:bg-gray-200 duration-150 :focus:ring-primary-800">Upload</button>
                   
               </form>
           </div>
